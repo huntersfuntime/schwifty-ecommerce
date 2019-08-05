101d56e5be3375897af1f638fc0401026767c0d3
Hunter Orges <hunterorges@gmail.com>
2019-08-05 13:47:35 -0600

    Add selectors to all dispatches to optimize components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
-
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -10,6 +9,9 @@ import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/auth/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+
+import "./App.css";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -58,8 +60,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
