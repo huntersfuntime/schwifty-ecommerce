cf399326c2fe0472fabb405c492ba208298abd26
Hunter Orges <hunterorges@gmail.com>
2019-08-07 15:02:47 -0600

    Add ability to dynamically change the routes

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-const CategoryPage = ({ match }) => (
+const CategoryPage = ({ collection }) => (
   <div className="collection">
     <h2>Collection Page</h2>
   </div>
 );
 
-export default CategoryPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CategoryPage);
