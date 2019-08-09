13d2f04e706005b2e0a94c1f15c4840db34747cd
Hunter Orges <hunterorges@gmail.com>
2019-08-09 16:05:04 -0600

    Add shop items to firebase

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SignInAndSignUpPage from "./pages/auth/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 
