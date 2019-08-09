7c148b85a5fe52b7611fd3e776d0850dcfcac84d
Hunter Orges <hunterorges@gmail.com>
2019-08-09 17:02:33 -0600

    Create a Spinner HOC

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,10 +1,8 @@
-import SHOP_DATA from "./shop.data";
-
 import ShopActionsTypes from "./shop.types";
 import ShopActionTypes from "./shop.types";
 
 const INITIAL_STATE = {
-  collections: SHOP_DATA
+  collections: null
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
